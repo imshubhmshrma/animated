eae13f8e783bb0d140da8f8dd7e957f5fa7172d7
Shubham Sharma <41778516+imshubhmshrma@users.noreply.github.com>
2018-08-06 18:14:32 +0530

    Create AnimatedView.swift

---
+++ AnimatedView.swift
@@ -0,0 +1,117 @@
+//
+//  AnimatedView.swift
+//  
+//
+//  Created by Shubham on 02/08/18.
+//  Copyright © 2018 Shubham. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class AnimatedView: UIView {
+    
+    
+    @IBInspectable var circular : Bool = false{
+        didSet{
+            self.applyCornerRadius()
+        }
+    }
+    
+    @IBInspectable
+    var cornerRadius: CGFloat {
+        get {
+            return layer.cornerRadius
+        }
+        set {
+            layer.cornerRadius = newValue
+        }
+    }
+    
+    @IBInspectable var borderWidth: CGFloat {
+        get {
+            return layer.borderWidth
+        }
+        set {
+            layer.borderWidth = newValue
+        }
+    }
+    
+    @IBInspectable
+    var borderColor: UIColor? {
+        get {
+            if let color = layer.borderColor {
+                return UIColor(cgColor: color)
+            }
+            return nil
+        }
+        set {
+            if let color = newValue {
+                layer.borderColor = color.cgColor
+            } else {
+                layer.borderColor = nil
+            }
+        }
+    }
+    
+    @IBInspectable
+    var shadowRadius: CGFloat {
+        get {
+            return layer.shadowRadius
+        }
+        set {
+            layer.shadowRadius = newValue
+        }
+    }
+    
+    @IBInspectable
+    var shadowOpacity: Float {
+        get {
+            return layer.shadowOpacity
+        }
+        set {
+            layer.shadowOpacity = newValue
+        }
+    }
+    
+    @IBInspectable
+    var shadowOffset: CGSize {
+        get {
+            return layer.shadowOffset
+        }
+        set {
+            layer.shadowOffset = newValue
+        }
+    }
+    
+    @IBInspectable
+    var shadowColor: UIColor? {
+        get {
+            if let color = layer.shadowColor {
+                return UIColor(cgColor: color)
+            }
+            return nil
+        }
+        set {
+            if let color = newValue {
+                layer.shadowColor = color.cgColor
+            } else {
+                layer.shadowColor = nil
+            }
+        }        
+    }
+    func applyCornerRadius()
+    {
+        if(self.circular) {
+            self.layer.cornerRadius = self.bounds.size.height/2
+            self.layer.masksToBounds = true
+            //self.layer.borderColor = self.borderColor.cgColor
+            self.layer.borderWidth = CGFloat(self.borderWidth)
+        }else {
+            self.layer.cornerRadius = cornerRadius
+            self.layer.masksToBounds = true
+           // self.layer.borderColor = self.borderColor.cgColor
+            self.layer.borderWidth = CGFloat(self.borderWidth)
+        }
+    }
+}
